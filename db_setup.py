dd187a111fb3aee0d047d66c3f9e0fdf82fbfcd1
juniorta <ttjrbiz@gmail.com>
2026-07-02 03:40:59 -0400

    created: sqlite database setup file and created the actula db

---
+++ db_setup.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+#connect
+conn=sqlite3.connect("wis.db")
+c= conn.cursor()
+
+#create table
+c.execute(""" CREATE TABLE IF NOT EXISTS transacs
+          (id INTEGER PRIMARY KEY, date TEXT, stock TEXT, capital FLOAT)
+""")
+
+#try insert data
+# c.execute("INSERT INTO transacs (date, stock, capital) VALUES (datetime(),'AAPL',500.0)")
+# c.execute("DELETE FROM transacs WHERE id=1")
+
+#save
+conn.commit()
+conn.close()
+
